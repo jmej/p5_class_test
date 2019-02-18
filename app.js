8ead3f9df7042c9b4c7f0e4296c437bc0b126596
Jesse Mejia <jmejia@anestheticaudio.com>
2019-02-18 14:13:26 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+// The file system module
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+let server = http.createServer(handleRequest);
+server.listen(8080);
+
+function handleRequest(req, res) {
+  // What did we request?
+  let pathname = req.url;
+  
+  // If blank let's ask for index.html
+  if (pathname == '/') {
+    pathname = '/index.html';
+  }
+  
+  // Ok what's our file extension
+  let ext = path.extname(pathname);
+
+  // Map extension to file type
+  const typeExt = {
+    '.html': 'text/html',
+    '.js':   'text/javascript',
+    '.css':  'text/css'
+  };
+
+  // What is it?  Default to plain text
+  let contentType = typeExt[ext] || 'text/plain';
+
+  // Now read and write back the file with the appropriate content type
+  fs.readFile(__dirname + pathname,
+    function (err, data) {
+      if (err) {
+        res.writeHead(500);
+        return res.end('Error loading ' + pathname);
+      }
+      // Dynamically setting content type
+      res.writeHead(200,{ 'Content-Type': contentType });
+      res.end(data);
+    }
+  );
+}
+
+
